75743d4b6268e76640f07ce43066414177216c71
Afique Bertrand <afiquebertrand@gmail.com>
2025-02-16 07:44:08 -0400

    Completed COMP3385 Assignment 1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Afqiue Bertrand</h2>
+    <p>i'm a uni student that enjoys playing games, opening and exploring electronics and driving</p>
+    <img src="{{ url('/images/dice-1502706_640.jpg') }}" alt="Dice Image" width="200">
+@endsection
