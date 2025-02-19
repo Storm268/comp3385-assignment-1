1c48a268d467fffa634e89fe86afac5b4c6abf05
Afique Bertrand <afiquebertrand@gmail.com>
2025-02-18 23:21:57 -0400

    Last commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <h2>Afqiue Bertrand</h2>
-    <p>i'm a uni student that enjoys playing games, opening and exploring electronics and driving</p>
+    <p>i'm a univesity student that enjoys playing games, opening and exploring electronics and driving. I’m all about performance upgrades tuning, tweaking. For tech i love to I push them beyond what they were meant to do, whether it’s modding software, overclocking, or upgrading hardware. I like figuring things out, improving them, and making them my own </p>
     <img src="{{ url('/images/dice-1502706_640.jpg') }}" alt="Dice Image" width="200">
 @endsection
